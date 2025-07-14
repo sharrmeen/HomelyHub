62fc8374df28ca54790703f630e183af74c4f7ee
Sharmeen <sharmeenk2020@gmail.com>
2025-07-14 17:58:48 +0530

    Fix JWT cookie settings for cross-origin requests

--- frontend2/src/utils/axiosInstance.js
+++ frontend2/src/utils/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true, 
 });
 
